ab2e034c58811b824a4969a95be5fbf0ce625672
Luis Javier Toro Flores <luis.toro.f@usach.cl>
2022-09-15 21:08:38 -0300

    Se agrega el proyecto

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+import com.example.demo.services.UploadService;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private UploadService upload;
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @PostMapping("/cargar")
+    public String carga( @RequestParam("archivos") MultipartFile file, RedirectAttributes ms) {
+        upload.save(file);
+        ms.addFlashAttribute("mensaje", "Archivo guardado correctamente!!");
+        return "redirect:/";
+    }
+
+}
